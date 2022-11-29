b02ff390fe9a3d5c603caec9ee6dfd52d63510d1
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2022-11-29 18:19:39 -0300

    04.4 Splash Screen

--- src/rotas.js
+++ src/rotas.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Tab = createNativeStackNavigator();
 
+import SplashScreen from './telas/SplashScreen';
 import Onboarding from './telas/Onboarding';
 import Principal from './telas/Principal';
 import Detalhes from './telas/Detalhes';
@@ -12,9 +13,10 @@ export default function Rotas() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Onboarding" component={Onboarding} options={{ headerShown: false }}/>
-        <Tab.Screen name="Principal" component={Principal} options={{ headerShown: false }}/>
-        <Tab.Screen name="Detalhes" component={Detalhes}/>
+        <Tab.Screen name="SplashCreen" component={SplashScreen} options={{ headerShown: false }} />
+        <Tab.Screen name="Onboarding" component={Onboarding} options={{ headerShown: false }} />
+        <Tab.Screen name="Principal" component={Principal} options={{ headerShown: false }} />
+        <Tab.Screen name="Detalhes" component={Detalhes} />
       </Tab.Navigator>
     </NavigationContainer>
   );
